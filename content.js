71992e09b7e09b98dae7dfb174329dfaedf811e4
Emma Heizer <emmaheizer@Emmas-MacBook-Pro-2.local>
2017-08-01 14:32:29 -0400

    this is the first commit for Flow Tool

---
+++ content.js
@@ -0,0 +1,25 @@
+/ content.js
+chrome.runtime.onMessage.addListener(
+  function(request, sender, sendResponse) {
+    if( request.message === "clicked_browser_action" ) {
+      var firstHref = $("a[href^='http']").eq(0).attr("href");
+
+      console.log(firstHref);/
+
+      / This line is new!
+      chrome.runtime.sendMessage({"message": "open_new_tab", "url": firstHref});
+    }
+  }
+);/
+
+
+/
+
+
+chrome.browserAction.onClicked.addListener(function(tab) {
+    alert('working?');
+});
+
+/
+
+
